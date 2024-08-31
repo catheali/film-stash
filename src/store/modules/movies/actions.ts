74e6aa95d27cf9bd0e455b2f4ccdffaaec7ff3b9
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 20:58:00 -0300

    feat(store): add actions for movie module

--- src/store/modules/movies/actions.ts
+++ src/store/modules/movies/actions.ts
@@ -1,3 +1,32 @@
-export default {
-	
-}
+
+import { ActionTree, ActionContext } from 'vuex';
+import { RootState, MoviesState, Movie, ApiResponse } from '@/types';
+import apiService from '@/services/apiService';
+
+export const actions: ActionTree<MoviesState, RootState> = {
+
+	async fetchMovies({ commit }) {
+		try {
+		  const response = await apiService.getData('top_rated');
+		  const movies: ApiResponse = response.data;
+		  commit('setMovies', movies.results) 
+		  commit('setApiResponse', movies);
+		} catch (error) {
+		  console.error('Erro ao buscar filmes:', error.message);
+		  commit('setError', error)
+		}
+	},
+
+	async fetchImageUrl({ commit }) {
+		try {
+		  const response = await apiService.getData('configuration');
+		  const movies: ApiResponse = response.data;
+		  commit('setMovies', movies.results) 
+		  commit('setApiResponse', movies);
+		} catch (error) {
+		  console.error('Erro ao buscar filmes:', error.message);
+		  commit('setError', error)
+		}
+	},
+
+};
