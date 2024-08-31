da2cbd18740f9e1e1dcd97c0268adb5bcdf2cefd
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 20:57:31 -0300

    feat(store): add mutations for movie module

--- src/store/modules/movies/mutations.ts
+++ src/store/modules/movies/mutations.ts
@@ -1,3 +1,20 @@
-export default {
-	
-}
+import { MutationTree } from 'vuex';
+import { MoviesState, Movie, ApiResponse } from  '@/types';
+
+export const mutations: MutationTree<MoviesState> = {
+  setMovies(state, movies: Movie[]) {
+    state.allMovies = movies;
+  },
+  addFavoriteMovie(state, movie: Movie) {
+    state.favoriteMovies.push(movie);
+  },
+  removeFavoriteMovie(state, movieId: number) {
+    state.favoriteMovies = state.favoriteMovies.filter(movie => movie.id !== movieId);
+  },
+  setApiResponse(state, response: ApiResponse) {
+    state.apiResponse = response;
+  },
+  setError(state: MoviesState, error: { message: string; code: number }) {
+    state.error = error;
+  },
+};
