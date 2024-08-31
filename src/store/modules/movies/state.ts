9c98f357b7f4f2d5470607b550b45e95b4da5983
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 20:57:15 -0300

    feat(store): add state for movie module

--- src/store/modules/movies/state.ts
+++ src/store/modules/movies/state.ts
@@ -1,3 +1,12 @@
-export default {
-	
-}
+import { MoviesState } from '@/types';
+
+export const state: MoviesState = {
+  allMovies: [],
+  favoriteMovies: [],
+  apiResponse: null,
+  error: {
+	success: null,
+    message: null,
+    code: null,
+  }
+};
