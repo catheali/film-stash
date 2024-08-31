cca187eaf6833b904b1ed5c0506d637c35c681de
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 20:57:50 -0300

    feat(store): add getters for movie module

--- src/store/modules/movies/getters.ts
+++ src/store/modules/movies/getters.ts
@@ -1,3 +1,9 @@
-export default {
-	
-}
+import { GetterTree } from 'vuex';
+import { RootState, MoviesState } from  '@/types';
+
+export const getters: GetterTree<MoviesState, RootState> = {
+  allMovies: (state) => state.allMovies,
+  favoriteMovies: (state) => state.favoriteMovies,
+  getApiResponse: (state) => state.apiResponse,
+  getError: (state) => state.error,
+};
