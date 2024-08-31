2d286d960079b0adbe37c265dd2eb4c822452046
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 14:58:19 -0300

    feat: add axios instance with base URL configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
-const api: AxiosInstance = axios.create({
+const apiClient: AxiosInstance = axios.create({
 	baseURL: "https://api.themoviedb.org/3/movie/",
 	headers: {
 		'Content-Type': 'application/json',
 	  },
 })
-
