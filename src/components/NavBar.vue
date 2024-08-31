87f804534fae472bb89b297110d2da5aa0bb80a9
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 00:10:58 -0300

    feat: add navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,72 @@
 <template>
-	<div>
-			<header>
-				nav bar
-			</header>
-	</div>
+	<header>
+		<div class="logo">
+			<span>Film</span>
+			<span>Stash</span>
+		</div>
+		<nav>	
+			<ul>
+				<li><router-link to="/home">Home</router-link></li>
+				<li><router-link to="/my-favorites">My Favorite Movies</router-link></li>
+				<li><router-link to="/about">About</router-link></li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-	setup () {
-		
-
-		return {}
-	}
+	name: "NavBar"
 })
 </script>
 
-<style scoped>
+
+<style lang="scss" scoped>
+
+header {
+	display: flex;
+	justify-content: space-between;
+	color: $white;
+	padding: 1rem;
+	
+	.logo {
+		font-family: Limelight;
+		font-size: 2rem;
+	}
+
+	div {
+		display: flex;
+	}
+
+	nav {	
+			ul {
+				display: flex;
+				justify-content: space-evenly;
+				align-items: center;
+				gap:10px;
+				li {
+				list-style-type: none;
+			
+				}
+			}
+			
+		}
+
+		@media (max-width: 768px) {
+			background-color: #e0e0e0;
+			padding: 15px;
+		}
+
+		@media (max-width: 480px) {
+			background-color: #d0d0d0;
+			padding: 10px;
+		}
+	& {
+		@include cinematic-gradient(); 
+	}
+}
+
 
 </style>
