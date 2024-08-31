0e977a3f7566b3752dc0eca6dfe16b7785add537
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 16:40:59 -0300

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export interface MoviesState {
+  allMovies: Movie[];        
+  favoriteMovies: Movie[];  
+  apiResponse: ApiResponse | null; 
+}
+
+export interface RootState {
+  movies: MoviesState; 
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface ApiResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
