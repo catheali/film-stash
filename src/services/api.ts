ca573f450fd65d008e8c840cf3aaf3ad52e03503
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-08-31 20:33:10 -0300

    refactor:  remove files

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import type { AxiosInstance } from 'axios';
-
-const apiClient: AxiosInstance = axios.create({
-	baseURL: "https://api.themoviedb.org/3/movie/",
-	headers: {
-		'Content-Type': 'application/json',
-	  },
-})
