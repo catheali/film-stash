e3f74197342abb8689618182e2609fbeab94d041
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-09-01 14:44:24 -0300

    feat: add my favorite movies view

---
+++ src/views/MyFavoriteMovies.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="">
+	  <h1>Filmes Favoritos</h1>
+	</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+   
+  export default defineComponent({
+	name: 'MyFavoriteMovies',
+	setup() {
+	  
+	  return {
+	
+	  };
+	},
+  });
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
