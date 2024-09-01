daaf25ec11737f1bdd169d00389e8063b4447785
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-09-01 14:45:04 -0300

    feat: add Movie view

---
+++ src/views/MovieView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="">
+	  <h1>title movie</h1>
+
+	</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, onMounted, ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import { useStore } from 'vuex';
+
+   
+  export default defineComponent({
+	name: 'movie',
+	setup() {
+		const store = useStore();
+		const route = useRoute();
+		
+		onMounted(async () => {
+		const movieId = route.params.id as string; 
+			let response = store.dispatch('fetchDetails', movieId);
+		})
+		
+		return {}
+		},
+  });
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
