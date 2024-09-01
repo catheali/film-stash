cf9702faa0ce814fae0e1788bee6809de17cddc0
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-09-01 14:45:15 -0300

    feat: add About view

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="">
+	  <h1>About the project!</h1>
+	</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+   
+  export default defineComponent({
+	name: 'About',
+	setup() {
+	  
+		const favoriteMovie = ref([]);
+
+		const fetchMovies = async ()=>{
+
+		};
+		
+	  return {
+		favoriteMovie
+	  };
+	},
+  });
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
