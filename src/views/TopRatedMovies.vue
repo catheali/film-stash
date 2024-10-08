a4e52eaecaf0901106776812d823fe50d8c21792
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-10-08 17:15:13 -0300

    style: adjusts styles

--- src/views/TopRatedMovies.vue
+++ src/views/TopRatedMovies.vue
@@ -5,7 +5,7 @@
 		</div>
 		<Pagination :endpointContext="'top_rated'"/>
 		<div class="movie-gallery"  >
-			<MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+			<MovieCard v-for="movie in movies" :key="movie.id" :movie="movie"  />
 		</div>	
 		
 	</div>
@@ -16,13 +16,14 @@
   import MovieCard from '../components/cards/MovieCard.vue';
   import { useStore } from 'vuex';
   import Pagination from '@/components/Pagination.vue';
-  
+
   export default defineComponent({
 		components: { MovieCard, Pagination },
 		name: 'MoviesView',
 	setup() {
 		const store = useStore();
 		const currentPage = ref(1);
+
 		const loadData = () => {
 			const endpoint = `top_rated?page=${currentPage.value}`;
 			store.dispatch('fetchMovies', endpoint);
@@ -43,5 +44,26 @@
 </script>
   
 <style lang="scss" scoped>
-	@import "@/assets/styles/scss/app.scss";
+	.movies-view {
+	padding: 20px 0;
+	background: $background-dark;
+	overflow: hidden;
+  }
+  
+  .movie-gallery {
+	display: flex;
+	gap: 20px;
+	flex-wrap: wrap;
+	justify-content: center;
+	overflow-y: scroll;
+	scrollbar-width: none;
+	height: 35rem;
+	margin: 1rem 8rem;
+  }
+  
+  .title-movies {
+	text-align: center;
+	font-family: Inter, sans-serif;
+  }
+  
 </style>
