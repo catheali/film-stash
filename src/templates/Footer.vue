83be7b63d03616936b7d4b433161515edff7cce2
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-10-08 17:08:34 -0300

    refactor; move to  the templates directory

---
+++ src/templates/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+	<footer class="footer">
+	  <div class="container">
+		<p>&copy; 2024 FilmStash.</p>
+		<ul class="social-links">
+		  <li><a href="https://linkedin.com.br/in/alicia-alencar" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+		  <li><a href="https://github.com/catheali" target="_blank" rel="noopener noreferrer">Github</a></li>
+		</ul>
+	  </div>
+	  <div>
+		<p>&copy; This product uses the <a href="https://developer.themoviedb.org/docs/getting-started" target="_blank" rel="noopener noreferrer">TMDB API</a> but is not endorsed or certified by TMDB. All rights reserved.</p>
+	  </div>
+	</footer>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+	name: 'Footer',
+  });
+  </script>
+
+<style lang="scss">
+.footer {
+	color: $color_primmary;
+	padding: 20px 0;
+	text-align: center;
+  
+	.container {
+	  max-width: 1200px;
+	  margin: 0 auto;
+	  padding: 0 20px;
+	}
+  
+	p {
+	  margin: 0;
+	}
+  
+	.social-links {
+	  list-style-type: none;
+	  padding: 0;
+	  margin: 10px 0 0;
+  
+	  li {
+		display: inline;
+		margin-right: 10px;
+  
+		a {
+		  color: #fff;
+		  text-decoration: none;
+  
+		  &:hover {
+			text-decoration: underline;
+		  }
+		}
+	  }
+	}
+
+	& {
+		@include cinematic-gradient(); 
+	}
+  }
+</style>
---
+++ src/templates/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+	<header>
+		<div class="logo">
+			<router-link to="/">
+				<span>Film</span>
+				<span>Stash</span>
+			</router-link>
+		</div>
+		<nav>	
+			<ul>
+				<li><router-link to="/">Top Rated Movies</router-link></li>
+				<li><router-link to="/my-favorites">My Favorite Movies</router-link></li>
+				<li><router-link to="/about">About</router-link></li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: "NavBar"
+})
+</script>
+
+<style lang="scss">
+
+header {
+	display: flex;
+	justify-content: space-between;
+	color: $color_primmary;
+	padding: 1rem;
+	
+	.logo {
+		font-family: Inter;
+		font-size: 2rem;
+		color: $white;
+	}
+
+	div {
+		display: flex;
+	}
+
+	nav {	
+			ul {
+				display: flex;
+				justify-content: space-evenly;
+				align-items: center;
+				gap:10px;
+				li {
+				list-style-type: none;
+			
+				}
+			}
+			
+		}
+
+		@media (max-width: 768px) {
+			background-color: #e0e0e0;
+			padding: 15px;
+		}
+
+		@media (max-width: 480px) {
+			background-color: #d0d0d0;
+			padding: 10px;
+		}
+	& {
+		@include cinematic-gradient(); 
+	}
+}
+
+</style>
