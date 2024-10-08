fed38586ccd8bb38333243dabc823e69c5a99927
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-10-08 17:14:32 -0300

    feat: implements Pagination Component to TopRatedMovies

--- src/views/TopRatedMovies.vue
+++ src/views/TopRatedMovies.vue
@@ -3,13 +3,7 @@
 		<div class="title-movies">
 			<h1>Top Rated Movies</h1>
 		</div>
-		<div class="pagination">
-			<button @click="previousPage" :disabled="currentPage === 1">Previous</button>
-			<button v-for="page in visiblePages" :key="page" @click="goToPage(page)" :class="{ active: page === currentPage }">
-				{{ page }}
-			</button>
-			<button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
-		</div>
+		<Pagination :endpointContext="'top_rated'"/>
 		<div class="movie-gallery"  >
 			<MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
 		</div>	
@@ -21,10 +15,10 @@
   import { defineComponent, onMounted , computed, ref } from 'vue';
   import MovieCard from '../components/cards/MovieCard.vue';
   import { useStore } from 'vuex';
-
+  import Pagination from '@/components/Pagination.vue';
   
   export default defineComponent({
-		components: { MovieCard },
+		components: { MovieCard, Pagination },
 		name: 'MoviesView',
 	setup() {
 		const store = useStore();
@@ -35,34 +29,6 @@
 		};
 		
 		const movies = computed(() => store.getters.allMovies);
-		const totalPages = computed(() => store.getters.getTotalPages);
-		
-		const visiblePages = computed(() => {
-			const startPage = Math.max(1, currentPage.value - 1);
-			const endPage = Math.min(totalPages.value, startPage + 2);
-			return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
- 		 });
-
-		const goToPage = (page: number) => {
-		if (page >= 1 && page <= totalPages.value) {
-			currentPage.value = page;
-			loadData();
-		}
-		};
-
-		const nextPage = () => {
-		if (currentPage.value < totalPages.value) {
-			currentPage.value++;
-			loadData();
-		}
-		};
-
-		const previousPage = () => {
-		if (currentPage.value > 1) {
-			currentPage.value--;
-			loadData();
-		}
-		};
 		
 		onMounted(() => {
 			loadData();
@@ -70,12 +36,7 @@
 
 		return {
 			movies,
-			currentPage,
-			totalPages,
-			visiblePages,
-			goToPage,
-			nextPage,
-			previousPage
+			currentPage
 		}
 	}
 	});
